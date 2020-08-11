c12906c02d1dffe0c3e6afcc447e5ab1ff60465a
sanjaynela <ballacksanjay@hotmail.com>
2020-08-11 15:15:48 -0700

    Problem4

--- Aug/Problem4.java
+++ Aug/Problem4.java
@@ -1,11 +1,31 @@
 public class Problem4{
     public boolean containsDuplicate(int[] nums){
         //Naive linear search
+        // for(int i=0;i<nums.length;i++){
+        //     for(int j=0;j<i;j++){
+        //         if(nums[j] == nums[i]){
+        //             return true;
+        //         }
+        //     }
+        // }
+
+        //Sorting
+        /**The implementation here modifies the original array by sorting it. 
+         * In general, it is not a good practice to modify the input 
+         * unless it is clear to the caller that the input will be modified.  */
+
+        // Arrays.sort(nums);
+        // for(int i=0;i<nums.length-1;i++){
+        //     if(nums[i]==nums[i+1]){
+        //         return true;
+        //     }
+        // }
+
+        //Set
+        Set<Integer> set = new HashSet<Integer>(nums.length);
         for(int i=0;i<nums.length;i++){
-            for(int j=0;j<i;j++){
-                if(nums[j] == nums[i]){
-                    return true;
-                }
+            if(set.contains(nums[i])){
+                return true;
             }
         }
         return false;
